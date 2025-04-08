8a5079102add86cd1d797f218cc5c5fa3ddaee29
oiazri <boysas09@gmail.com>
2025-04-08 07:14:06 +0700

    addresbook

---
+++ README.md
@@ -0,0 +1,1 @@
+# ADDRES BOOK
---
+++ script.js
@@ -0,0 +1,18 @@
+const contacts = [
+  {
+    id: 1,
+    name: "saiful azri siregar",
+    age: 30,
+    email: "saifulazrisiregar@gmail.com",
+    phone: "+62-851-0193029",
+  },
+  {
+    id: 2,
+    name: "susanto",
+    age: 50,
+    email: "susanto@gmail.com",
+    phone: "+1-12121-0193029",
+  },
+];
+
+console.log(contacts);
