680994fa9524c379c66d2129e7ee6bd3806b2d6a
oiazri <boysas09@gmail.com>
2025-04-09 20:01:14 +0700

    string

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ const contacts = [
     name: "saiful azri siregar",
     age: 30,
     email: "saifulazrisiregar@gmail.com",
-    phone: 081210550085,
+    phone: "+6281210550085",
   },
   {
     id: 2,
     name: "susanto",
     age: 50,
     email: "susanto@gmail.com",
-    phone: 081210550085,
+    phone: "+6281210550085",
   },
 ];
 
