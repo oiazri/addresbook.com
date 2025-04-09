7f53049fd59b7a706d150859adcbd437e312754c
oiazri <boysas09@gmail.com>
2025-04-09 12:17:52 +0700

    adding no handphone

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ const contacts = [
     name: "saiful azri siregar",
     age: 30,
     email: "saifulazrisiregar@gmail.com",
-    phone: "+62-851-0193029",
+    phone: 081210550085,
   },
   {
     id: 2,
     name: "susanto",
     age: 50,
     email: "susanto@gmail.com",
-    phone: "+1-12121-0193029",
+    phone: 081210550085,
   },
 ];
 
