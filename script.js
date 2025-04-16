d2d28051d6765862807a2e5773dc49e0294b3baf
oiazri <boysas09@gmail.com>
2025-04-16 10:13:41 +0700

    add new contact

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 const contacts = [
   {
-    id: 1,
-    name: "Alex Praroro",
+    Id: 1,
+    name: "Alexandro Carlos",
     age: 30,
-    email: "alexpraroro@gmail.com",
+    email: "alexandrocarlos@gmail.com",
     phone: "+6281210550085",
     address: "Jakarta",
     country: "Indonesia",
   },
   {
-    id: 2,
-    name: "Susanto",
+    Id: 2,
+    name: "Minato",
     age: 50,
-    email: "susanto@gmail.com",
+    email: "minato@gmail.com",
     phone: "+81210550085",
     address: "Osaka",
     country: "Japan",
   },
   {
-    id: 3,
-    name: "Muhammad Ronaldo",
-    age: 39,
-    email: "mronaldo@gmail.com",
+    Id: 3,
+    name: "Rico Puji",
+    age: 33,
+    email: "ricopuji@gmail.com",
     phone: "+319742640382",
     address: "Cibubur",
-    country: "Argentina",
+    country: "Jakarta",
   },
 ];
 
@@ -33,6 +33,7 @@ function listContacts() {
     const contact = contacts[index];
 
     console.log(`
+Id: ${contact.Id}      
 Name: ${contact.name}
 Age: ${contact.age} year old
 Email: ${contact.email}
@@ -42,5 +43,24 @@ Country: ${contact.country}
 `);
   }
 }
+
+ function addContact(name, age, email, phone, address, country) {
+  const lastContact = contacts[contacts.length - 1];
+  const lastId = lastContact.Id;
+  const nextId = lastId + 1;
+
+  contacts.push({
+    Id: nextId,
+    name,
+    age,
+    email,
+    phone,
+    address,
+    country,
+  });
+}
+
+addContact("siregar", 30, "siregar@gmail.com", "Jakarta", "+628122066789","Indonesia" )
+addContact("Muhammad Raihan", 21, "m.raihan@gmail.com", "Banda Aceh", "+62232244", "Indonesia" )
 
 listContacts();
