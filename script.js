b3cc4a6657150cde50085affb2bf0c51828d0be2
M Haidar Hanif <git@mhaidarhanif.com>
2025-04-16 20:34:50 +0700

    demo code

--- script.js
+++ script.js
@@ -60,6 +60,10 @@ function addContact(name, age, email, phone, address, country) {
   contacts.push(newContact);
 }
 
+function searchContacts() {
+  // TODO: Use filter
+}
+
 addContact(
   "Siregar",
   30,
